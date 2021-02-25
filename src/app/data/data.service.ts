b03e90395d903c6366779fae0bccbbce6ab91eef
Alexander Dimitrow <adimitrow@vmware.com>
2021-02-25 22:56:42 +0200

    added comments on how to setup a dummy server to test our post call

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -13,6 +13,7 @@ export class DataService {
   // we will send a form to a web server using PutsReq
   postUserSettingsForm(userSettings: UserSettings): Observable<any>{
      // return of(userSettings); => testing our service
+     // For getting a test url got to https://putsreq.com , get your URL and test the post call
     return this.http.post('https://putsreq.com/00bRJNNCchhoc9HlXBKX',userSettings);
   }
 }
